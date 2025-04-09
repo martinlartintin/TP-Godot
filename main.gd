4e1b242e394dfbd807a775fd5daa506754cdaf9b
martinlartintin <benitezmartineugenio@gmail.com>
2025-04-09 12:05:38 -0300

    pantalla frontal

--- main.gd
+++ main.gd
@@ -26,16 +26,25 @@ func Game_over() -> void:
 	pass 
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+
+	get_tree().call_group("mobs", "queue_free")
+
 
 func _on_score_timer_timeout() -> void:
-	pass
 	score += 1
+
+	$HUD.update_score(score)
+
+	pass
 
 func _on_start_timer_timeout() -> void:
 	pass
