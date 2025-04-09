f0cf6a0ee081b9bf103cceaab78809ba6c7a063b
martinlartintin <benitezmartineugenio@gmail.com>
2025-04-09 12:13:31 -0300

    finalizando el juego

--- main.gd
+++ main.gd
@@ -28,6 +28,9 @@ func Game_over() -> void:
 	$MobTimer.stop()
 	$HUD.show_game_over()
 
+	$Music.stop()
+	$DeathSound.play()
+
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
@@ -37,6 +40,8 @@ func new_game():
 	$HUD.show_message("Get Ready")
 
 	get_tree().call_group("mobs", "queue_free")
+
+	$Music.play()
 
 
 func _on_score_timer_timeout() -> void:
